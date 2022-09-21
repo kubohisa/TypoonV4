64c518aa29079eeae6fce8ca677fc0e9c67c16ee
kubohisa <kubohisa@gmail.com>
2022-09-21 20:04:11 +0900

    Fixess.

--- Typoon/FilePost.php
+++ Typoon/FilePost.php
@@ -4,15 +4,15 @@ class FilePost
 {
 	public static function judge() {$file} {
 		// https://magicen.wikipedia.org/wiki/List_of_file_signatures
-		$magic = file_get_contents($filename, false, null, 0, 12);
+		$magic = file_get_contents($file, false, null, 0, 12);
 		
 		$ext = "";
 		
-		if (preg_match("#^\xFF[\xF3\xFB\xFA\xF2]#", $magic) || preg_match("#^ID3#", $magic)) {
+		if (preg_match("\A\xFF[\xF3\xFB\xFA\xF2]#", $magic) || preg_match("#\AID3#", $magic)) {
 			$ext = 'mp3';
-		} elseif (preg_match("#^OggS#", $magic)) {
+		} elseif (preg_match("#\AOggS#", $magic)) {
 			$ext = 'ogg';
-		} elseif (preg_match("#^ftyp#", $magic)) {
+		} elseif (preg_match("#\Aftyp#", $magic)) {
 			$ext = 'mpeg4';
 		}
 		
