30a4ad17571e0ff048506d631f736e9f29f3c46c
kubohisa <kubohisa@gmail.com>
2022-09-22 02:59:00 +0900

    Update FilePost.php

--- Typoon/FilePost.php
+++ Typoon/FilePost.php
@@ -1,9 +1,20 @@
 <?php
 
+/*
+$_FILES['userfile']['name']
+$_FILES['userfile']['size']
+$_FILES['userfile']['tmp_name']
+$_FILES['userfile']['error']
+
+is_uploaded_file()
+move_uploaded_file()
+
+*/
+
 class FilePost
 {
 	public static function judge() {$file} {
-		// https://magicen.wikipedia.org/wiki/List_of_file_signatures
+		// https://en.wikipedia.org/wiki/List_of_file_signatures
 		$magic = file_get_contents($file, false, null, 0, 12);
 		
 		$ext = "";
@@ -13,9 +24,41 @@ class FilePost
 		} elseif (preg_match("#\AOggS#", $magic)) {
 			$ext = 'ogg';
 		} elseif (preg_match("#\Aftyp#", $magic)) {
-			$ext = 'mpeg4';
+			$ext = 'm4a';
 		}
 		
 		return $ext;
+	}
+
+	public static function getName($name) {
+		return basename($_FILES[$name]['name'];
+	}
+
+	public static function checkExt() {$file, $exts} {
+		$path_parts = pathinfo($file);
+		$ext = $path_parts['extension'];
+		
+		$exts = explode("|", $exts);
+		
+		foreach($exts as $key => $value) {
+			$value = trim($value);
+			
+			if ($ext === $value) return true;
+		}
+		
+		return false;
+	}
+
+	public static function upload() {$name, $dir} {
+		//
+		if (! is_uploaded_file($_FILES[$name]['tmp_name'])) return false;
+		if ($_FILES[$name]['error'] !== 0) return false;
+		
+		//
+		if (move_uploaded_file($_FILES[$name]['tmp_name'], $dir.basename($_FILES[$name]['name']))) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
