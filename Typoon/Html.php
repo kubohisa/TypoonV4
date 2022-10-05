88c8012a0a639cfb51c9b1a8d0e5302a3411df17
kubohisa <kubohisa@gmail.com>
2022-10-05 12:02:31 +0900

    Update Html.php

--- Typoon/Html.php
+++ Typoon/Html.php
@@ -12,4 +12,78 @@ class Html
         global $EXEC;
         echo new div("../App/Action/{$EXEC}/".$file, $data);
     }
+
+    public static function file($outputfile, $file, $data)
+    {
+		if (! file_exists($outputfile)) return false;
+		
+		file_put_contents($outputfile, new div($file, $data), LOCK_EX);
+		return true;
+    }
+
+	public static function json($data)
+    {
+		header("Content-type: application/json");
+        echo json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+		exit;
+    }
+
+	/*
+	
+	*/
+	
+	public static function mimeAudio($ext) {
+		$ext = trim($ext, "\.");
+		
+		$mime = "";
+		
+		switch($ext)
+		{
+			case 'ogg':
+				$mime = "audio/ogg";
+				break;
+			case 'mp3':
+				$mime = "audio/mpeg";
+				break;
+			case 'm4a':
+				$mime = "audio/aac";
+				break;
+			default;
+				break;		
+		}
+		
+		return $mime;
+	}
+
+	public static function fileput($file) {
+		if (! file_exists($file)) return false;
+		
+		preg_match("#\.(.+)\z#", $file, $ext);
+		$ext = DataFile::mimeAudio($ext[1]);
+		
+		$mime = "";
+		
+		switch($ext)
+		{
+			case 'ogg':
+				$mime = "audio/ogg";
+				break;
+			case 'mp3':
+				$mime = "audio/mpeg";
+				break;
+			case 'm4a':
+				$mime = "audio/aac";
+				break;
+			default;
+				break;		
+		}
+		
+		if ($mime !== "") {
+			header("Content-type: {$mime}");
+		}
+		
+		echo(file_get_contents($file));
+		
+		return true;
+	}
 }
