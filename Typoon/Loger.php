f04c5b47a7aebbc185c7eb721313e8287d802427
kubohisa <kubohisa@gmail.com>
2022-10-05 12:51:51 +0900

    Create Loger.php

---
+++ Typoon/Loger.php
@@ -0,0 +1,13 @@
+<?php
+
+class Loger {
+	public static function log($text) {
+		$file = '../App/Log/'.date('Ym').'.log';
+
+		error_log("[".date('Y-m-d H:i:s')."] ".$text."\n",
+			3,
+			$file);
+	}
+}
+
+
