8f9d957707fea1b9daf91325ab2c859217612f40
kubohisa <kubohisa@gmail.com>
2022-10-12 13:08:24 +0900

    Update Login.php

--- Typoon/Login.php
+++ Typoon/Login.php
@@ -5,7 +5,7 @@ class Login
     public static function set($id)
     {
         $_SESSION['LoginId'] = $id;
-        $_SESSION['LoginToken'] = hash('ripemd160', microtime());
+        $_SESSION['LoginToken'] = hash('ripemd160', $id.$_SERVER['REMOTE_ADDR'].microtime().rand());
     }
 
     public static function check()
@@ -17,19 +17,19 @@ class Login
         header("Location: /logout");
         exit;
     }
-	
+
     public static function logout()
     {
-/*        // Delete Session.
-        $_SESSION = array();
-        if (isset($_COOKIE["PHPSESSID"])) {
-            setcookie("PHPSESSID", '', time() - 1800, '/');
-        }
-        session_destroy();
+        /*        // Delete Session.
+                $_SESSION = array();
+                if (isset($_COOKIE["PHPSESSID"])) {
+                    setcookie("PHPSESSID", '', time() - 1800, '/');
+                }
+                session_destroy();
 
-        // Go TopPage.
-        $URI = "/";
-*/
+                // Go TopPage.
+                $URI = "/";
+        */
         header("Location: /logout");
         exit;
     }
