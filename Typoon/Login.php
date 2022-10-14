c2d364a0f2949eb45202519c3940eb0356a7d2b5
kubohisa <kubohisa@gmail.com>
2022-10-14 12:04:46 +0900

    Update Login.php

--- Typoon/Login.php
+++ Typoon/Login.php
@@ -5,13 +5,20 @@ class Login
     public static function set($id)
     {
         $_SESSION['LoginId'] = $id;
-        $_SESSION['uuId'] = hash('sha512', 
-									hash('sha512', $id)."_".
-									hash('sha512', "NameSpace")."_".
-								//	hash('sha512', $_SERVER['REMOTE_ADDR'])."_".
-									hash('sha512', microtime())."_".
-									hash('sha512', rand())
-								);
+
+        //
+        $_SESSION['uuId'] = strtoupper(hash(
+            'sha512',
+            hash('sha512', $id)."_".
+            hash('sha512', "NameSpace")."_".
+        //	hash('sha512', $_SERVER['REMOTE_ADDR'])."_".
+            hash('sha512', microtime())."_".
+            hash('sha512', rand())
+        ));
+
+        //
+        $_SESSION['guId'] = substr(substr_replace($_SESSION['uuId'], 'A', 16, 1), 0, 32);
+        $_SESSION['guId'] = preg_replace('#\A(.{8})(.{4})(.{4})(.{4})(.{12})#', '$1-$2-$3-$4-$5', $_SESSION['guId']);
     }
 
     public static function check()
